ac78c55755b6826b2ce6bcbe0a876e5c6adbbcc0
Pankaj Lohani <116501381+PlGehu@users.noreply.github.com>
2023-06-08 13:25:47 -0500

    Create Square_of_n_numbers.py

---
+++ Programs/Square_of_n_numbers.py
@@ -0,0 +1,22 @@
+n = int(input("Enter range "))
+i=1
+
+while (i <= n):
+   num=int(input("Enter number "))
+   print(num*num)
+   i = i+1
+
+    
+    
+#OUTPUT
+'''
+Enter range 4
+Enter number 4
+16
+Enter number 8
+64
+Enter number 9
+81
+Enter number 0
+0
+'''
