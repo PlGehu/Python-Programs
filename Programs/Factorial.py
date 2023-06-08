27cb007d2a5b626ab12e238a745aea59d906a4ca
Pankaj Lohani <116501381+PlGehu@users.noreply.github.com>
2023-06-07 19:23:06 -0500

    Create Factorial.py

---
+++ Programs/Factorial.py
@@ -0,0 +1,16 @@
+n=int(input("Enter any number "))
+i=1
+fact=1
+
+while (i<=n):
+    fact=fact*i
+    i=i+1
+
+print("The factorial of " ,n, "is ", fact)
+
+'''
+OUTPUT :-
+Enter any number 6
+The factorial of  6 is  720
+
+'''
